16f9db578ce787f854e50a3d11e2fbe58cf0735b
Moneyflies <eremchuk.boris@gmail.com>
2024-03-04 09:13:47 +0300

    create class for pixels

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.getElementById('container');
 const containerSize = container.offsetHeight;
-const div = document.createElement('div');
+const pixel = document.createElement('div');
+pixel.classList.add('pixel');
+const colors = ['blue', 'red', 'green', 'purple', 'grey', 'white', 'black', 'pink', 'yellow', 'brown', 'aqua', 'lime'];
 let i = 0;
 while (i <= 16) {
     container.appendChild(div);
