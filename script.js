2928d8c4dcaacca4671bd2660073b9b0e047c663
Moneyflies <eremchuk.boris@gmail.com>
2024-03-04 09:42:20 +0300

    add event for pixels

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ for (let i = 0; i <= 16; i++) {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
     container.appendChild(pixel);
+    pixel.addEventListener("mouseover", () => {
+        setColor(pixel);
+    });
+}
+
+function setColor(el) {
+    const color = getRandomColor();
+    el.style.background = color;
+}
+
+function random(min, max) {
+    return min + Math.random() * (max - min);
+  }
+
+function getRandomColor () {
+    return colors.random(0, colors.length - 1);
 }
