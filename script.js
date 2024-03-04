3441691d27f07e897c8ee6efeea25c6c72020cbe
Moneyflies <eremchuk.boris@gmail.com>
2024-03-04 11:20:59 +0300

    fix some problems and realize events

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-const container = document.getElementById('container');
-const containerSize = container.offsetHeight;
-const colors = ['blue', 'red', 'green', 'purple', 'grey', 'white', 'black', 'pink', 'yellow', 'brown', 'aqua', 'lime'];
-for (let i = 0; i <= 16; i++) {
-    const pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    container.appendChild(pixel);
-    pixel.addEventListener("mouseover", () => {
-        setColor(pixel);
-    });
+const container = document.querySelector('.container');
+const colors = ['blue', 'red', 'green', 'purple', 'grey', 'white', 'pink', 'yellow', 'brown', 'aqua', 'lime'];
+let pixelCount = +prompt('Choose size grid: ', '16');
+
+container.style.gridTemplateColumns = `repeat(${pixelCount}, 1fr)`;
+container.style.gridTemplateRows = `repeat(${pixelCount}, 1fr)`;
+
+for (let i = 0; i < pixelCount; i++) {
+    for (let j = 0; j < pixelCount; j++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        container.appendChild(pixel);
+        pixel.addEventListener("mouseover", () => {
+            setColor(pixel);
+        });
+    }
 }
 
 function setColor(el) {
-    const color = getRandomColor();
+    const color = colors[Math.floor(Math.random() * colors.length - 1)];
     el.style.background = color;
-}
-
-function random(min, max) {
-    return min + Math.random() * (max - min);
-  }
-
-function getRandomColor () {
-    return colors.random(0, colors.length - 1);
 }
